24f86b1ef39c62a0378ccd779404c3d8a9c661e7
Lucas-Mosca <110690274+Lucas-Mosca@users.noreply.github.com>
2023-07-07 23:12:02 -0300

    Feature: First project structure.

---
+++ app/src/main/java/br/com/alarm/app/base/BaseFragment.kt
@@ -0,0 +1,123 @@
+package br.com.alarm.app.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import br.com.alarm.app.R
+import com.google.android.material.snackbar.Snackbar
+
+@Suppress("UNCHECKED_CAST")
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    abstract val bindingInflater: (LayoutInflater) -> VB
+    private var viewBinding: ViewBinding? = null
+
+    val binding: VB
+        get() = viewBinding as VB
+    private var currentToast: Toast? = null
+    private var currentSnackBar: Snackbar? = null
+
+    abstract val viewModel: BaseViewModel
+    abstract fun initViews()
+    abstract fun initObservers()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = bindingInflater.invoke(inflater)
+        return viewBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) {
+            onErrorMessage(it)
+        }
+
+
+        initObservers()
+        initViews()
+    }
+
+    private fun setStatusBarColor(@ColorRes color: Int) {
+        requireActivity().findViewById<CoordinatorLayout>(R.id.clAppRoot)
+            .setBackgroundColor(requireContext().getColor(color))
+    }
+
+    fun setStatusBarColorFromPalette(@ColorInt colorInt: Int) {
+        requireActivity().findViewById<CoordinatorLayout>(R.id.clAppRoot)
+            .setBackgroundColor(colorInt)
+    }
+
+    open fun onErrorMessage(it: Any?) {
+        when (it) {
+            is Int -> showShortToast(it)
+            is String -> showShortToast(it)
+        }
+    }
+
+    private fun showShortToast(@StringRes stringResId: Int) {
+        showShortToast(getString(stringResId))
+    }
+
+    fun showLongToast(@StringRes stringResId: Int) {
+        showLongToast(getString(stringResId))
+    }
+
+    fun showShortToast(message: String) {
+        showToast(message, Toast.LENGTH_SHORT)
+    }
+
+    fun showLongSnackBar(message: String) {
+        showSnackBar(message, Snackbar.LENGTH_LONG)
+    }
+
+    fun showShortSnackBar(message: String) {
+        showSnackBar(message, Snackbar.LENGTH_SHORT)
+    }
+
+    fun showLongToast(message: String) {
+        showToast(message, Toast.LENGTH_LONG)
+    }
+
+    fun setBackNavigation(onBackPressed: () -> Unit) {
+        requireActivity().onBackPressedDispatcher.addCallback {
+            onBackPressed.invoke()
+        }
+    }
+
+
+    private fun showToast(message: String, duration: Int) {
+        currentToast?.cancel()
+        currentToast = Toast.makeText(context, message, duration)
+        currentToast?.show()
+    }
+
+    private fun showSnackBar(message: String, duration: Int) {
+        currentSnackBar?.dismiss()
+        currentSnackBar =
+            Snackbar.make(binding.root, message, duration)
+                .setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.purple_200))
+                .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+        currentSnackBar?.show()
+    }
+
+    override fun onDestroyView() {
+        viewBinding = null
+        super.onDestroyView()
+    }
+
+}
---
+++ app/src/main/java/br/com/alarm/app/screen/alarm/AlarmViewModel.kt
@@ -0,0 +1,9 @@
+package br.com.alarm.app.screen.alarm
+
+import br.com.alarm.app.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmViewModel @Inject constructor() : BaseViewModel() {
+}
