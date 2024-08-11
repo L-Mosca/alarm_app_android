83eac163e63f168bbbd1dbb6bb2f1b644eded06b
Lucas Mosca <lucasbvmosca@gmail.com>
2024-08-11 16:24:21 -0300

    Feature: Add notification service and receiver service.

--- app/src/main/java/br/com/alarm/app/domain/service/NotificationReceiver.kt
+++ app/src/main/java/br/com/alarm/app/domain/service/NotificationReceiver.kt
@@ -3,14 +3,12 @@ package br.com.alarm.app.domain.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import br.com.alarm.app.domain.service.NotificationService.Companion.NOTIFICATION_DEFAULT_ID
 import br.com.alarm.app.domain.service.NotificationService.Companion.NOTIFICATION_EXTRA
 
-class NotificationReceiver: BroadcastReceiver() {
+class NotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
-        val data = intent?.getLongExtra(NOTIFICATION_EXTRA, NOTIFICATION_DEFAULT_ID)
-        Log.e("test", "chegou no receiver: $data")
+        intent?.getLongExtra(NOTIFICATION_EXTRA, NOTIFICATION_DEFAULT_ID)
     }
 }
