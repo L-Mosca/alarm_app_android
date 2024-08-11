1ff7daeb4de6f14134446dc6f516868403663d63
Lucas Mosca <lucasbvmosca@gmail.com>
2024-08-11 16:21:01 -0300

    Feature: Add notification service and receiver service.

--- app/src/main/java/br/com/alarm/app/AlarmApp.kt
+++ app/src/main/java/br/com/alarm/app/AlarmApp.kt
@@ -1,8 +1,14 @@
 package br.com.alarm.app
 
 import android.app.Application
+import br.com.alarm.app.domain.service.NotificationService
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AlarmApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        NotificationService.createNotificationChannel(this)
+    }
 }
--- app/src/main/java/br/com/alarm/app/host/HostActivity.kt
+++ app/src/main/java/br/com/alarm/app/host/HostActivity.kt
@@ -1,20 +1,23 @@
 package br.com.alarm.app.host
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import br.com.alarm.app.R
 import br.com.alarm.app.databinding.ActivityHostBinding
+import br.com.alarm.app.domain.service.NotificationService
+import br.com.alarm.app.domain.service.NotificationService.Companion.NOTIFICATION_DEFAULT_ID
+import br.com.alarm.app.domain.service.NotificationService.Companion.NOTIFICATION_EXTRA
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HostActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHostBinding
+    private val viewModel: HostViewModel by viewModels()
+    private lateinit var notificationService: NotificationService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,21 +25,27 @@ class HostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_host)
         binding = ActivityHostBinding.bind(findViewById(R.id.navHostContainer))
 
-        createNotificationChannel()
+        notificationService = NotificationService(this)
 
+        initObservers()
+
+
+        if (intent.hasExtra(NOTIFICATION_EXTRA)) {
+            Log.e(
+                "test",
+                "DADOS RECEBIDOS: ${
+                    intent?.getLongExtra(
+                        NOTIFICATION_EXTRA,
+                        NOTIFICATION_DEFAULT_ID
+                    )
+                }"
+            )
+        }
     }
 
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Channel Name"
-            val descriptionText = "Channel Description"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel("channel_id", name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+    private fun initObservers() {
+        viewModel.createNotification.observe(this) {
+            notificationService.showNotification(it)
         }
     }
 }
